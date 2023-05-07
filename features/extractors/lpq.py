0849f143607319306c87ac9c1433b5eaaa2e47b5
Melikapyn <120106396+Melikapyn@users.noreply.github.com>
2023-05-07 17:15:44 +0200

    create pftas lpq and pftas

--- features/extractors/lpq.py
+++ features/extractors/lpq.py
@@ -1,101 +1,78 @@
-import cv2
+from __future__ import division
+
 import numpy as np
-from scipy.fftpack import fft2, fftshift
-from skimage.util import view_as_windows
+from scipy.signal import convolve2d
+import cv2
+import matplotlib.pyplot as plt
 
-class LocalPhaseQuantization:
-    def __init__(self, M=3, Np=8):
-        self.M = M
-        self.Np = Np
+class LPQ:
+    def __init__(self, winSize=3, freqestim=1, mode='nh'):
+        self.winSize = winSize
+        self.freqestim = freqestim
+        self.mode = mode
+        self.rho = 0.90
+        self.STFTalpha = 1/winSize
+        self.sigmaS = (winSize-1)/4
+        self.sigmaA = 8/(winSize-1)
+        self.convmode = 'valid'
 
-    def __get_local_phase(self, patch):
-        # Compute short-term Fourier transform
-        f = fft2(patch)
-        f = fftshift(f)
+    def compute_STFT_filters(self, x):
+        if self.freqestim == 1:
+            # Basic STFT filters
+            w0 = np.ones_like(x)
+            w1 = np.exp(-2*np.pi*x*self.STFTalpha*1j)
+            w2 = np.conj(w1)
+        return w0, w1, w2
 
-        # Get magnitude and phase of Fourier coefficients
-        magnitude = np.abs(f)
-        phase = np.angle(f)
+    def compute_LPQdesc(self, img):
+        img = np.float64(img)
+        r = (self.winSize-1)/2
+        x = np.arange(-r,r+1)[np.newaxis]
+        w0, w1, w2 = self.compute_STFT_filters(x)
 
-        # Apply Gaussian weighting to magnitude
-        sigma = self.M / 6.4
-        weight = np.exp(-(np.arange(-self.M//2, self.M//2+1)**2)/(2*sigma**2))
-        weight = np.outer(weight, weight)
-        if magnitude.shape != weight.shape:
-            h, w = magnitude.shape
-            m = (h - weight.shape[0]) // 2
-            n = (w - weight.shape[1]) // 2
-            magnitude = magnitude[m:m+weight.shape[0], n:n+weight.shape[1]]
-        magnitude *= weight
+        # Run filters to compute the frequency response in the four points. Store real and imaginary parts separately
+        # Run first filter
+        filterResp1 = convolve2d(convolve2d(img,w0.T,self.convmode),w1,self.convmode)
+        filterResp2 = convolve2d(convolve2d(img,w1.T,self.convmode),w0,self.convmode)
+        filterResp3 = convolve2d(convolve2d(img,w1.T,self.convmode),w1,self.convmode)
+        filterResp4 = convolve2d(convolve2d(img,w1.T,self.convmode),w2,self.convmode)
 
-        # Compute the average phase
-        sum_sin = np.sum(magnitude * np.sin(phase))
-        sum_cos = np.sum(magnitude * np.cos(phase))
-        avg_phase = np.arctan2(sum_sin, sum_cos)
+        # Initialize frequency domain matrix for four frequency coordinates (real and imaginary parts for each frequency).
+        freqResp = np.dstack([filterResp1.real, filterResp1.imag,
+                              filterResp2.real, filterResp2.imag,
+                              filterResp3.real, filterResp3.imag,
+                              filterResp4.real, filterResp4.imag])
 
-        # Convert the phase to [0, 2*pi]
-        if avg_phase < 0:
-            avg_phase += 2 * np.pi
+        # Perform quantization and compute LPQ codewords
+        inds = np.arange(freqResp.shape[2])[np.newaxis,np.newaxis,:]
+        LPQdesc = ((freqResp>0)*(2**inds)).sum(2)
 
-        return avg_phase
+        # Switch format to uint8 if LPQ code image is required as output
+        if self.mode == 'im':
+            LPQdesc = np.uint8(LPQdesc)
 
-    def __get_binary_code(self, phase):
-        # Convert phase to 8-bit integer
-        phase = np.uint8(phase * 255 / (2*np.pi))
+        # Histogram if needed
+        if self.mode == 'nh' or self.mode == 'h':
+            LPQdesc = np.histogram(LPQdesc.flatten(),range(256))[0]
 
-        # Convert phase to binary code
-        binary_code = np.zeros((8,), dtype=np.uint8)
-        for i in range(8):
-            binary_code[i] = np.uint8((phase >> i) & 1)
+        # Normalize histogram if needed
+        if self.mode == 'nh':
+            LPQdesc = LPQdesc/LPQdesc.sum()
 
-        return binary_code
+        return LPQdesc
 
-    def describe(self, image):
-        # Convert image to grayscale
-        if len(image.shape) == 3 and image.shape[2] == 3:
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+if __name__ == "__main__":
 
-        # Pad the image with zeros to avoid boundary effects
-        pad_size = self.M // 2
-        image = cv2.copyMakeBorder(image, pad_size, pad_size, pad_size, pad_size, cv2.BORDER_CONSTANT, value=0)
+    # Load image and convert to grayscale
+    image = cv2.imread('/Users/melikapooyan/Downloads/BreaKHis_v1/breast/benign/SOB/adenosis/SOB_B_A_14-22549AB/40X/SOB_B_A-14-22549AB-40-003.png')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-        # Compute short-term Fourier transform for each patch
-        patches = view_as_windows(image, (self.M, self.M), step=1)
-        phases = np.zeros(patches.shape[:2])
-        for i in range(patches.shape[0]):
-            for j in range(patches.shape[1]):
-                phases[i, j] = self.__get_local_phase(patches[i, j])
-
-        # Compute binary codes for each patch
-        binary_codes = np.zeros(patches.shape[:2] + (8,), dtype=np.uint8)
-        for i in range(patches.shape[0]):
-         for j in range(patches.shape[1]):
-          binary_codes[i, j] = self.__get_binary_code(phases[i, j])
-        # Accumulate binary codes in histogram
-        hist = np.zeros((256,), dtype=np.float32)
-        for i in range(binary_codes.shape[0]):
-         for j in range(binary_codes.shape[1]):
-          idx = int(''.join([str(b) for b in binary_codes[i, j]]), 2)
-          hist[idx] += 1
-
-        # Normalize histogram
-        hist /= np.sum(hist)
-
-if __name__ == '__main__':
- import matplotlib.pyplot as plt
-
-image = cv2.imread('/path/to/image.png')
-lpq = LocalPhaseQuantization()
-hist = lpq.describe(image)
-
-# Plot histogram
-# plt.bar(range(len(hist)), hist)
-#plt.title('LPQ Histogram')
-# plt.xlabel('Binary code')
-# plt.ylabel('Normalized frequency')
-# plt.show()
-
-
+    # Compute LPQ descriptor and histogram
+    lpq = LPQ()
+    lpq_desc = lpq.compute_LPQdesc(gray)
+    hist = plt.hist(lpq_desc, bins=256)
+    print (lpq_desc)
+    print(hist[0])
 
 
 
