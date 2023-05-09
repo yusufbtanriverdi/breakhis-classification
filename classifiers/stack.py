28f4aa2e483d64f3d454a0b59b752ec8656d704a
yusuftengriverdi <yusuftengriverdi@github.com>
2023-05-09 16:42:20 +0200

    go back feature_extraction

--- classifiers/stack.py
+++ classifiers/stack.py
@@ -27,7 +27,7 @@ def read_features(extractors, root='./features/all/', mode='binary', mf='40X'):
         featuredir = basedir + str(extractors[0]) + '.csv'
         csv = pd.read_csv(featuredir)
         return [csv['image'], csv.iloc[:, 2:], csv['label']]
-    
+
 
     for extractor in extractors:
         featuredir = basedir + str(extractor) + '.csv'
