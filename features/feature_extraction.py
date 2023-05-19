56713daf724f4b02acd0d171b503993e6e913905
hadiiiil <rg.hadil87@gmail.com>
2023-05-19 19:27:44 +0200

    feature extractor

--- features/feature_extraction.py
+++ features/feature_extraction.py
@@ -32,7 +32,7 @@ sys.path.append(parent_dir)
 from classifiers.stack import read_data
 from torchvision import transforms
 
-def extract_features(stacks, extractors=None, save=True, feature_dir="features/all/binary/400X/"):
+def extract_features(stacks, extractors=None, save=True, feature_dir="features/all/binary/40X/"):
     """Extract features from input images using specified feature extractors."""
     
     # Get number of samples and number of feature extractors.
@@ -68,20 +68,21 @@ def extract_features(stacks, extractors=None, save=True, feature_dir="features/a
 
 if __name__ == "__main__":
     extractors = [# LocalBinaryPatterns(8, 1), 
-                #   LPQ(radius=3, neighbors=8, block_size=3),
+                  # LPQ(radius=3, neighbors=8, block_size=3),
                   # GLCM(distances=[1], angles=[0, np.pi/4, np.pi/2, 3*np.pi/4], levels=256),
                   # ORB(num_keypoints=500),
-                  CLBP(radius=5, neighbors=24),
-                #   PFTAS()
+                  # CLBP(radius=5, neighbors=24),
+                  # PFTAS()
                   # FOS(),
                   # HOS(),
-                #   FFT(),
+                  # FFT(),
                   ]
 
-    stack  = read_data(root="C:/Users/hadil/Documents/projects/Machine Learning/project/breast/", mf='400X', mode='binary',shuffle=False)
+    mf = '40X'
+    stack  = read_data(root="C:/Users/hadil/Documents/projects/Machine Learning/project/breast/", mf=mf, mode='binary',shuffle=False)
     if len(stack) == 0:
         print("Please change data dir!!")
         raise NotADirectoryError
     
-    mf = '400X'
+    
     fnames, df = extract_features(stack, extractors=extractors, save=True, feature_dir=f'features/all/binary/{mf}/')
