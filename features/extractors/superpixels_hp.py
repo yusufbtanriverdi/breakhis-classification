3d1bdb131533c545decfccd43d9de6bd78297b69
Melikapyn <120106396+Melikapyn@users.noreply.github.com>
2023-05-19 16:24:33 +0200

    in progress

--- features/extractors/superpixels_hp.py
+++ features/extractors/superpixels_hp.py
@@ -1,8 +1,7 @@
-# 
 import cv2 as cv
 import numpy as np
 
-class SuperpixelsEx:
+class Superpixels:
     def __init__(self, algorithm='SLIC'):
         self.algorithm = algorithm
         self.str_to_algorithm = {
@@ -29,15 +28,17 @@ class SuperpixelsEx:
             img_clustered[class_mask != 0, :] = mean_color[:3]
         return img_clustered, img_superpixeled, labels, num_superpixels
 
-    def describe(self, img):
+    def extract_features(self, img):
         # Extract color features (mean values for each channel)
         color_features = np.mean(img, axis=(0, 1))
+
         # Extract shape features (using Hu Moments)
         gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         moments = cv.moments(gray_img)
         hu_moments = cv.HuMoments(moments)
         shape_features = -np.sign((hu_moments) * np.log10(np.abs(hu_moments)))
         shape_features = shape_features.reshape(-1)
+
         return np.concatenate([color_features, shape_features])
 
 if __name__ == '__main__':
@@ -45,15 +46,17 @@ if __name__ == '__main__':
     image = cv.imread(image_path, cv.IMREAD_COLOR)
     image = cv.cvtColor(image, cv.COLOR_BGR2Lab)
 
-    my_superpixels = SuperpixelsEx(algorithm='SLIC')
+    my_superpixels = Superpixels(algorithm='SLIC')
     img_clustered, img_superpixeled, labels, num_superpixels = my_superpixels.superpixels(image)
 
     all_features = []
     for k in range(num_superpixels):
         class_mask = (labels == k).astype("uint8")
         img_clustered_masked = cv.bitwise_and(img_clustered, img_clustered, mask=class_mask)
-        features = my_superpixels.describe(img_clustered_masked)
+        features = my_superpixels.extract_features(img_clustered_masked)
         all_features.append(features)
 
     all_features = np.array(all_features)
+
+
 
